268d6e1c334e8e4519c10e82e576473b762ab152
gooddwarf <gooddwarf42@gmail.com>
2024-12-01 20:38:41 +0100

    const

--- hello-world.ts
+++ hello-world.ts
@@ -1,9 +1,9 @@
 import {AOC01} from "./01/01";
 
-let welcomeMessage: string = 'I\'m running, rawr!!';
+const welcomeMessage: string = 'I\'m running, rawr!!';
 //let message2: string = 4;
 console.log(welcomeMessage);
-var puzzle = new AOC01();
+const puzzle = new AOC01();
 const input = puzzle.readInput();
 puzzle.partOne(input);
 puzzle.partTwo(input);

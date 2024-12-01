3c6fa8424629c86e8b465b4a242345c4b199af6e
gooddwarf <gooddwarf42@gmail.com>
2024-12-01 19:42:14 +0100

    part one

--- 01/01.ts
+++ 01/01.ts
@@ -1,26 +1,50 @@
 import * as fs from 'fs';
 
 export class AOC01 {
-    private _test: boolean = true;
+    private _test: boolean = false;
     private _inputFile: string = this._test
         ? './01/testInput.txt'
         : './01/input.txt';
 
-    private readInput() : void {
-        const data = fs.readFileSync(this._inputFile, 'utf-8');
-        console.log('File content:', data);
+    public readInput(): string {
+        const input = fs.readFileSync(this._inputFile, 'utf-8');
+        console.log('input read!')
+        return input;
     }
 
-    public partOne(): void {
-        let message: string = 'Hello, yu!';
+    public partOne(input: string): void {
+        console.log('Solving part one...');
+        const parsedInput = this.parseInput(input);
+        parsedInput.firstColumn.sort();
+        parsedInput.secondColumn.sort();
 
-        //let message2: string = 4;
-        console.log(message);
-        this.readInput();
+        let distance = 0;
+        for (let i = 0; i < parsedInput.length; i++) {
+            distance += Math.abs(parsedInput.firstColumn[i] - parsedInput.secondColumn[i])
+        }
+
+        console.log(distance);
     }
 
-
-    public partTwo(): void {
+    public partTwo(input: string): void {
+        console.log('Solving part two...');
         console.log("TODO");
+    }
+
+    private parseInput(input: string): { firstColumn: number[], secondColumn: number[], length: number } {
+        // Yes, I know, I could have read the stream from the file one line at the time,
+        // but let me play around a bit first
+        const lines = input.split('\n');
+
+        const firstColumn: number[] = [];
+        const secondColumn: number[] = [];
+        lines.forEach((line) => {
+            var numbers = line.split(/\s+/);
+            firstColumn.push(parseInt(numbers[0]));
+            secondColumn.push(parseInt(numbers[1]));
+        })
+
+        console.log('input parsed!')
+        return {firstColumn, secondColumn, length: firstColumn.length};
     }
 }
--- hello-world.ts
+++ hello-world.ts
@@ -4,5 +4,6 @@ let welcomeMessage: string = 'I\'m running, rawr!!';
 //let message2: string = 4;
 console.log(welcomeMessage);
 var puzzle = new AOC01();
-puzzle.partOne();
-puzzle.partTwo();
+const input = puzzle.readInput();
+puzzle.partOne(input);
+puzzle.partTwo(input);

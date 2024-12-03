09f68f30bc751800828a9d41e726fdbec725bdc5
Marco Girardi <marco.girardi@factorymind.com>
2024-12-03 12:47:01 +0100

    set up

--- hello-world.ts
+++ hello-world.ts
@@ -4,7 +4,7 @@ import {AOC02} from "./02/02";
 const welcomeMessage: string = 'I\'m running, rawr!!';
 //let message2: string = 4;
 console.log(welcomeMessage);
-const puzzle = new AOC02();
+const puzzle = new AOC03();
 const input = puzzle.readInput();
 puzzle.partOne(input);
 puzzle.partTwo(input);

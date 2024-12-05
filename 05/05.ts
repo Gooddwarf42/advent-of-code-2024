7b0ede58832157673b9c4330e3b73627f4c35c88
gooddwarf <gooddwarf42@gmail.com>
2024-12-05 22:25:29 +0100

    part 2

--- 05/05.ts
+++ 05/05.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 export class AOC05 {
     private _day: string = '05';
-    private _test: boolean = true;
+    private _test: boolean = false;
     private _inputFile: string = this._test
         ? `./${this._day}/testInput.txt`
         : `./${this._day}/input.txt`;
@@ -18,33 +18,14 @@ export class AOC05 {
 
         const parsedInput = this.parseInput(input);
 
-        const isCorrect = (update: number[]): boolean => {
-            const indexes = new Map<number, number>();
-            for (const [index, pageNumber] of update.entries()) {
-                indexes.set(pageNumber, index);
-            }
-
-            for (const rule of parsedInput.rules) {
-                const firstIndex = indexes.get(rule.first);
-                const secondIndex = indexes.get(rule.second);
-                if (firstIndex === undefined
-                    || secondIndex === undefined
-                    || firstIndex < secondIndex) {
-                    continue;
-                }
-                return false;
-            }
-
-            return true;
-        }
-
         let count = 0;
-        for (let i = 0; i < parsedInput.updates.length; i++) {
-            const update = parsedInput.updates[i];
-            if (isCorrect(update)) {
-                const middle = update[(update.length - 1) / 2];
-                count += middle;
+        for (const update of parsedInput.updates) {
+            if (!this.isCorrect(update, parsedInput.rules)) {
+                continue;
             }
+
+            const middle = this.getMiddleOfOddSizedArray(update);
+            count += middle;
         }
 
         console.log(count);
@@ -55,47 +36,21 @@ export class AOC05 {
 
         const parsedInput = this.parseInput(input);
 
-        const rules = parsedInput.rules;
-
-        const rulesWithOccurrencesOnRight: (Rule & { rightOccurrences: number }) [] = [];
-        for (const rule of rules) {
-            const rightOccurrences = rules.filter(r => r.second === rule.first).length;
-            rulesWithOccurrencesOnRight.push({...rule, rightOccurrences});
-        }
-        console.log(rulesWithOccurrencesOnRight);
-
-        const theTruth: number [] = [];
-        let copyOfRules = [...rules];
-
-        while (copyOfRules.length > 0) {
-            console.log(copyOfRules)
-            let brokeOut = false;
-
-            if (copyOfRules.length === 1) {
-                theTruth.push(copyOfRules[0].first, copyOfRules[0].second);
-                console.log("done!");
-                break;
+        let count = 0;
+        for (const update of parsedInput.updates) {
+            if (this.isCorrect(update, parsedInput.rules)) {
+                continue;
             }
 
-            for (const rule of copyOfRules) {
-                const rightOccurrences = copyOfRules.filter(r => r.second === rule.first).length;
-                if (rightOccurrences === 0) {
-                    console.log(rule.first + 'is good!');
-                    theTruth.push(rule.first);
-                    copyOfRules = copyOfRules.filter(r => r.first !== rule.first);
-                    brokeOut = true;
-                    break;
-                }
-            }
+            const relevantRules = parsedInput.rules.filter(r => update.includes(r.first) && update.includes(r.second));
+            const theTruth = this.findTruth(relevantRules);
 
-            if (!brokeOut) {
-                console.log('WTF');
-                console.log(copyOfRules);
-                break;
-            }
+            // first attempt that will not work. But actually does thanks to input analysis.
+            const middle = this.getMiddleOfOddSizedArray(theTruth);
+            count += middle;
         }
 
-        console.log(theTruth);
+        console.log(count);
     }
 
     private parseInput(input: string): Gigi {
@@ -110,6 +65,48 @@ export class AOC05 {
         });
         const updates = rawUpdates.split('\n').map<number[]>(line => line.split(',').map(s => parseInt(s)));
         return {rules, updates};
+    }
+
+    private isCorrect = (update: number[], rules: Rule[]): boolean => {
+        const indexes = new Map<number, number>();
+        for (const [index, pageNumber] of update.entries()) {
+            indexes.set(pageNumber, index);
+        }
+
+        for (const rule of rules) {
+            const firstIndex = indexes.get(rule.first);
+            const secondIndex = indexes.get(rule.second);
+            if (firstIndex === undefined
+                || secondIndex === undefined
+                || firstIndex < secondIndex) {
+                continue;
+            }
+            return false;
+        }
+
+        return true;
+    }
+
+    private findTruth(rules: Rule[]): number[] {
+        const theTruth: number [] = [];
+        let copyOfRules = [...rules];
+
+        while (copyOfRules.length > 1) {
+            for (const rule of copyOfRules) {
+                const rightOccurrences = copyOfRules.filter(r => r.second === rule.first).length;
+                if (rightOccurrences === 0) {
+                    theTruth.push(rule.first);
+                    copyOfRules = copyOfRules.filter(r => r.first !== rule.first);
+                    break;
+                }
+            }
+        }
+        theTruth.push(copyOfRules[0].first, copyOfRules[0].second);
+        return theTruth;
+    }
+
+    private getMiddleOfOddSizedArray(array: number[]): number {
+        return array[(array.length - 1) / 2];
     }
 }
 

82b8a4765ca9ae51ce6f693eb313f90b153cb7bf
gooddwarf <gooddwarf42@gmail.com>
2024-12-07 23:04:49 +0100

    setup

---
+++ 07/07.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+
+export class AOC07 {
+    private _day: string = '07';
+    private _test: boolean = true;
+    private _inputFile: string = this._test
+        ? `./${this._day}/testInput.txt`
+        : `./${this._day}/input.txt`;
+
+    public readInput(): string {
+        const input = fs.readFileSync(this._inputFile, 'utf-8');
+        console.log('input read!')
+        return input;
+    }
+
+    public partOne(input: string): void {
+        console.log('Solving part one...');
+
+        const parsedInput = this.parseInput(input);
+
+        console.log('TODO');
+    }
+
+    public partTwo(input: string): void {
+        console.log('Solving part two...');
+
+        const parsedInput = this.parseInput(input);
+        console.log('TODO');
+    }
+
+    private parseInput(input: string): string[] {
+
+        return input.split('\n');
+    }
+
+}

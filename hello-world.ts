9ae898cdbe1faa0c96a721deeaae47f40c024dcc
gooddwarf <gooddwarf42@gmail.com>
2024-12-08 18:05:21 +0100

    it works. Very unclean

--- hello-world.ts
+++ hello-world.ts
@@ -4,11 +4,12 @@ import {AOC03} from "./03/03";
 import {AOC04} from "./04/04";
 import {AOC05} from "./05/05";
 import {AOC07} from "./07/07";
+import {AOC08} from "./08/08";
 
 const welcomeMessage: string = 'I\'m running, rawr!!';
 //let message2: string = 4;
 console.log(welcomeMessage);
-const puzzle = new AOC07();
+const puzzle = new AOC08();
 const input = puzzle.readInput();
 puzzle.partOne(input);
 puzzle.partTwo(input);

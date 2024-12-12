30a2c0cba69f331416b1e1321052fb01c0a406ea
Marco Girardi <marco.girardi@factorymind.com>
2024-12-12 13:53:06 +0100

    part1

--- 12/12.ts
+++ 12/12.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 
 export class AOC12 {
     private _day: string = '12';
-    private _test: boolean = true;
+    private _test: boolean = false;
     private _inputFile: string = this._test
         ? `./${this._day}/testInput.txt`
         : `./${this._day}/input.txt`;
@@ -18,7 +18,14 @@ export class AOC12 {
 
         const parsedInput = this.parseInput(input);
 
-        console.log('TODO');
+        const fences = this.computeFences(parsedInput);
+
+        const cost = fences.reduce((acc: number, current) => {
+            acc += (current.area * current.perimeter);
+            return acc;
+        }, 0)
+
+        console.log(cost);
     }
 
     public partTwo(input: string): void {
@@ -33,4 +40,84 @@ export class AOC12 {
         return input.split('\n');
     }
 
+    private computeFences(parsedInput: string[]): RegionInfo[] {
+        const fences: RegionInfo[] = [];
+
+        const map: string[][] = [...parsedInput].map(s => s.split(''));
+        const visitedMap: (string | '.')[][] = [...parsedInput].map(s => s.split(''));
+        for (let i = 0; i < visitedMap.length; i++) {
+            for (let j = 0; j < visitedMap[i].length; j++) {
+                const regionIdentifier = visitedMap[i][j];
+
+                if (regionIdentifier === '.') {
+                    continue;
+                }
+
+                //console.log(`Found region to compute! ${regionIdentifier} at (${i},${j})`);
+                const areaData = this.visitArea(regionIdentifier, i, j, map, visitedMap) as RegionInfo;
+
+                //console.log(areaData);
+                fences.push(areaData);
+            }
+        }
+        return fences;
+    }
+
+    function
+
+    printMap(visitedMap: string[][]): string {
+        return visitedMap.map(row => row.join('')).join('\n')
+    }
+
+    private visitArea(regionIdentifier: string, i: number, j: number, map: string[][], visitedMap: (string | ".")[][]): VisitResult {
+
+        const identifier = map?.[i]?.[j];
+        const visited = visitedMap?.[i]?.[j];
+        if (identifier === undefined) {
+            return 'nonrelevant';
+        }
+        if (identifier != regionIdentifier) {
+            return 'nonrelevant';
+        }
+        if (visited === '.') {
+            return 'visited';
+        }
+
+        // We are in a proper place of this region. Mark this as visited and continue exploring
+        visitedMap[i][j] = '.';
+
+        const neighboursResults = [
+            this.visitArea(regionIdentifier, i - 1, j, map, visitedMap),
+            this.visitArea(regionIdentifier, i, j + 1, map, visitedMap),
+            this.visitArea(regionIdentifier, i + 1, j, map, visitedMap),
+            this.visitArea(regionIdentifier, i, j - 1, map, visitedMap),
+        ];
+        
+        const nonrelevantNeighbours = neighboursResults.filter(r => r === 'nonrelevant');
+        const relevantNeighbours = neighboursResults.filter(r => r !== 'nonrelevant' && r !== 'visited') as RegionInfo[];
+
+        if (relevantNeighbours.length === 0) {
+            return {
+                area: 1,
+                perimeter: nonrelevantNeighbours.length
+            }
+        }
+
+        const area = relevantNeighbours.reduce((acc: number, current) => {
+            acc += current.area;
+            return acc;
+        }, 1);
+        const perimeter = relevantNeighbours.reduce((acc: number, current) => {
+            acc += current.perimeter;
+            return acc;
+        }, nonrelevantNeighbours.length);
+        return {area, perimeter};
+    }
+}
+
+type VisitResult = RegionInfo | 'visited' | 'nonrelevant';
+type RegionInfo = { area: number, perimeter: number };
+
+function printMap(visitedMap: string[][]): string {
+    return visitedMap.map(row => row.join('')).join('\n')
 }

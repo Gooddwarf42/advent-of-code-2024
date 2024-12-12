2681a6dea4a89a45e78657c7126418afc7ad4bd9
gooddwarf <gooddwarf42@gmail.com>
2024-12-12 01:45:59 +0100

    Comparisons

--- 11/11.ts
+++ 11/11.ts
@@ -35,12 +35,39 @@ export class AOC11 {
 
         const rules = [rule1, rule2, rule3];
 
+        console.log('fully iterative version');
         console.log(this.blink(parsedInput, rules, 25).length);
+
+        console.log('recursion version');
+        const startRec = performance.now();
+        let count = 0;
+        for (const entry of parsedInput) {
+            count += this.countStonesGeneratedFromThisAfterBlinksWithNoMemorization(entry, 25, rules);
+        }
+        const endRec = performance.now();
+        console.log(`"Done. ElapsedTime: ${endRec - startRec}ms`);
+        console.log(count);
+
+
+        console.log('recursion with memorization version');
+        const start = performance.now();
+        let countWithMemorization = 0;
+        for (const entry of parsedInput) {
+            countWithMemorization += this.countStonesGeneratedFromThisAfterBlinks(entry, 25, rules);
+        }
+        const end = performance.now();
+        console.log(`"Done. ElapsedTime: ${end - start}ms`);
+        console.log(countWithMemorization);
+        console.log(this._cacheHits);
     }
 
     public partTwo(input: string): void {
         console.log('Solving part two...');
-        console.log('ACTUALLY doing part1 again');
+
+        this._cacheHits = 0;
+        this._stonesCache.clear();
+
+
         const parsedInput = this.parseInput(input);
 
         const rule1: Rule = {
@@ -61,14 +88,31 @@ export class AOC11 {
 
         const rules = [rule1, rule2, rule3];
 
+        console.log('fully iterative version');
+        console.log('omitted because it scales horribly (35-40 blinks took 45s)');
+        //console.log(this.blink(parsedInput, rules, 75).length);
+
+        console.log('recursion version');
+        console.log('omitted because it scales horribly and takes forever');
+        // const startRec = performance.now();
+        // let count = 0;
+        // for (const entry of parsedInput) {
+        //     count += this.countStonesGeneratedFromThisAfterBlinksWithNoMemorization(entry, 75, rules);
+        // }
+        // const endRec = performance.now();
+        // console.log(`"Done. ElapsedTime: ${endRec - startRec}ms`);
+        // console.log(count);
+
+
+        console.log('recursion with memorization version');
         const start = performance.now();
-        let count = 0;
+        let countWithMemorization = 0;
         for (const entry of parsedInput) {
-            count += this.countStonesGeneratedFromThisAfterBlinks(entry, 25, rules);
+            countWithMemorization += this.countStonesGeneratedFromThisAfterBlinks(entry, 75, rules);
         }
         const end = performance.now();
         console.log(`"Done. ElapsedTime: ${end - start}ms`);
-        console.log(count);
+        console.log(countWithMemorization);
         console.log(this._cacheHits);
     }
 
@@ -105,11 +149,11 @@ export class AOC11 {
 
     private countStonesGeneratedFromThisAfterBlinks(entry: number, remainingBlinks: number, rules: Rule[]) {
 
-        let cachedResults = this.stonesCache.get(entry);
+        let cachedResults = this._stonesCache.get(entry);
         // initialize cachedResult[entry]
         if (cachedResults === undefined) {
-            this.stonesCache.set(entry, new Map<number, number>());
-            cachedResults = this.stonesCache.get(entry);
+            this._stonesCache.set(entry, new Map<number, number>());
+            cachedResults = this._stonesCache.get(entry);
         }
         const properCachedResult = cachedResults!.get(remainingBlinks);
 
@@ -144,7 +188,33 @@ export class AOC11 {
         throw Error("something bad happened");
     }
 
-    private stonesCache: Map<number, Map<number, number>> = new Map<number, Map<number, number>>();
+
+    private countStonesGeneratedFromThisAfterBlinksWithNoMemorization(entry: number, remainingBlinks: number, rules: Rule[]) {
+
+        // base case here!
+        if (remainingBlinks === 0) {
+            return 1;
+        }
+
+        // meat of the logic here
+        for (const rule of rules) {
+            if (!rule.condition(entry)) {
+                continue;
+            }
+
+            const nextStepStones = rule.effect(entry);
+
+            let count = 0;
+            for (const stone of nextStepStones) {
+                count += this.countStonesGeneratedFromThisAfterBlinksWithNoMemorization(stone, remainingBlinks - 1, rules);
+            }
+            return count;
+        }
+
+        throw Error("something bad happened");
+    }
+
+    private _stonesCache: Map<number, Map<number, number>> = new Map<number, Map<number, number>>();
 }
 
 type Rule = {

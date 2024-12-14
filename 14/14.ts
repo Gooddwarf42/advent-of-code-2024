b4eac807e5ba7af7eaaaf9eb641a58752b4e0e0f
gooddwarf <gooddwarf42@gmail.com>
2024-12-14 11:43:29 +0100

    Not very elegant, but it does work nicely it seems. Fuck putting the right variables in the right checks

--- 14/14.ts
+++ 14/14.ts
@@ -1,36 +1,123 @@
 import * as fs from 'fs';
+import {NiceModulo, Sum, VectorClass} from "../Shared/shared";
 
 export class AOC14 {
     private _day: string = '14';
-    private _test: boolean = true;
+    private _test: boolean = false;
     private _inputFile: string = this._test
         ? `./${this._day}/testInput.txt`
         : `./${this._day}/input.txt`;
 
     public readInput(): string {
+
         const input = fs.readFileSync(this._inputFile, 'utf-8');
         console.log('input read!')
         return input;
     }
 
     public partOne(input: string): void {
+
         console.log('Solving part one...');
 
         const parsedInput = this.parseInput(input);
 
-        console.log('TODO');
+        const mapWidth = this._test ? 11 : 101;
+        const mapHeight = this._test ? 7 : 103;
+
+        const widthMiddleIndex = (mapWidth - 1) / 2
+        const heightMiddleIndex = (mapHeight - 1) / 2
+
+        const checkQuadrant = (position: VectorClass): 'TL' | 'TR' | 'BL' | 'BR' | null => {
+            if (position.x < 0
+                || position.x >= mapWidth
+                || position.y < 0
+                || position.y >= mapHeight) {
+                throw new Error("Invalid position");
+            }
+
+            if (position.x < widthMiddleIndex) {
+                if (position.y < heightMiddleIndex) {
+                    return 'TL'
+                }
+                if (position.y > heightMiddleIndex) {
+                    return 'BL'
+                }
+                return null;
+            }
+
+            if (position.x > widthMiddleIndex) {
+                if (position.y < heightMiddleIndex) {
+                    return 'TR'
+                }
+                if (position.y > heightMiddleIndex) {
+                    return 'BR'
+                }
+                return null;
+            }
+            return null;
+        }
+
+        const quadrantCountMap = new Map<'TL' | 'TR' | 'BL' | 'BR', number>();
+        quadrantCountMap.set('TL', 0);
+        quadrantCountMap.set('TR', 0);
+        quadrantCountMap.set('BL', 0);
+        quadrantCountMap.set('BR', 0);
+
+        const seconds = 100;
+        for (const robot of parsedInput) {
+
+            const finalPosition = robot.startingPosition
+                .Sum(robot.velocity.Multiply(seconds));
+            finalPosition.x = NiceModulo(finalPosition.x, mapWidth);
+            finalPosition.y = NiceModulo(finalPosition.y, mapHeight);
+
+            const quadrant = checkQuadrant(finalPosition);
+            if (quadrant === null) {
+                continue;
+            }
+
+            const countToIncrement = quadrantCountMap.get(quadrant) as number;
+            quadrantCountMap.set(quadrant, countToIncrement + 1)
+        }
+
+
+        let count = 1;
+        quadrantCountMap.forEach(value => count *= value)
+        console.log(count);
     }
 
     public partTwo(input: string): void {
+
         console.log('Solving part two...');
 
         const parsedInput = this.parseInput(input);
         console.log('TODO');
     }
 
-    private parseInput(input: string): string {
+    private parseInput(input: string): Robot[] {
 
-        return input;
+        const robots: Robot[] = [];
+        const lines = input.split('\n');
+
+        for (const line of lines) {
+
+            const parts = line.split(' ');
+
+            const extractVector = (s: string): VectorClass => {
+                const data = s.substring(2).split(',').map(s => parseInt(s));
+                return new VectorClass(data[0], data[1]);
+            };
+
+            const robot: Robot = {
+                startingPosition: extractVector(parts[0]),
+                velocity: extractVector(parts[1]),
+            };
+            robots.push(robot);
+        }
+
+        return robots;
     }
 
 }
+
+type Robot = { startingPosition: VectorClass, velocity: VectorClass };
--- Shared/shared.ts
+++ Shared/shared.ts
@@ -8,3 +8,49 @@ export function groupBy<T, K extends keyof any>(array: T[], keySelector: (item: 
         return acc;
     }, {} as Record<K, T[]>);
 }
+
+export type Vector = { x: number, y: number };
+
+export class VectorClass {
+    public x: number;
+    public y: number;
+
+    public constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Sum(vector: VectorClass): VectorClass {
+        return new VectorClass(this.x + vector.x, this.y + vector.y);
+    }
+
+    public Multiply(scalar: number): VectorClass {
+        return new VectorClass(this.x * scalar, this.y * scalar);
+    }
+
+}
+
+export function SumVectors(v1: Vector, v2: Vector): Vector {
+    return {
+        x: v1.x + v2.x,
+        y: v1.y + v2.y,
+    }
+}
+
+export function Sum(v1: Vector, v2: Vector): Vector {
+    return {
+        x: v1.x + v2.x,
+        y: v1.y + v2.y,
+    }
+}
+
+export function NiceModulo(input: number, modulo: number): number {
+    if (modulo <= 0) {
+        throw new Error("Bad modulo");
+    }
+
+    const standardModuloResult = input % modulo;
+    return standardModuloResult >= 0
+        ? standardModuloResult
+        : standardModuloResult + modulo;
+}
